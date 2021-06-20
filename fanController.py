f7e42d5a085442e77f9a5b78917dfc24aab0fe88
Craig McCoy <craig.graham.mccoy@gmail.com>
2021-06-20 00:19:41 -0600

    Added supported for retrieving and storing configuration data from/to an ini file.

--- fanController.py
+++ fanController.py
@@ -7,12 +7,21 @@ from time import monotonic, sleep
 import RPi.GPIO as GPIO
 from gpiozero import CPUTemperature
 from simple_pid import PID
+import configparser
 
 TACH_PIN = 17
 FAN_PWM_PIN = 18
 FAN_PWM_FEQUENCY = 1000
+DUTY_CYCLE_MIN = 0
+DUTY_CYCLE_MAX = 100
 
-DESIRED_TEMPERATURE = 35  # In celsius
+PID_SAMPLE_TIME = 1.0  # In seconds 
+
+# Default PID arguments...actual values to be retrieved from the ini file
+desired_temperature = 35  # In celsius
+kp = 10.0
+ki = 1.0
+kd = 0.5
 
 degree_sign = "℃"
 up_arrow = "▲"
@@ -30,10 +39,14 @@ def controlLoop():
     GPIO.setup(TACH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(FAN_PWM_PIN, GPIO.OUT)
     # Set up the PID object
-    pid = PID(-10, -1, -0.5)
-    pid.setpoint = DESIRED_TEMPERATURE
-    pid.output_limits = (0, 100)
-    pid.sample_time = 1.0
+    kp = getPidKValue('p')
+    ki = getPidKValue('i')
+    kd = getPidKValue('d')
+    print(f'kp = {kp}; ki = {ki}; kd = {kd}')
+    pid = PID(-kp, -ki, -kd)  # use negative value sinces we want to have the reverse effect (i.e. cooling)
+    pid.setpoint = desired_temperature
+    pid.output_limits = (DUTY_CYCLE_MIN, DUTY_CYCLE_MAX)
+    pid.sample_time = PID_SAMPLE_TIME
     # Set up the CPU Temperature object
     cpu = CPUTemperature()
     # Set up PWM on the Fan Pin
@@ -54,11 +67,11 @@ def controlLoop():
         current_temperature = cpu.temperature
         lbl_temperature["text"] = f'Temperature = {current_temperature:.2f}' + degree_sign
 
-        pid.setpoint = DESIRED_TEMPERATURE
+        pid.setpoint = desired_temperature
         fan_duty_cycle = pid(current_temperature)
         lbl_duty_cycle["text"] = f'Duty Cycle = {fan_duty_cycle:.0f}%'
         pwm.ChangeDutyCycle(fan_duty_cycle)
-        lbl_rpm["text"] = f'RPMs = {current_tach_counter * 30:d} rpm'
+        lbl_rpm["text"] = f'Fan Running at {current_tach_counter * 30:d} rpm'
 
         sleep_time = 1.0 - (monotonic() - begin_time)
         sleep(sleep_time)
@@ -68,27 +81,48 @@ def controlLoop():
     print("Exiting the Control Loop")
 
 def increase():
-    global DESIRED_TEMPERATURE
-    if (DESIRED_TEMPERATURE < 100):
-        DESIRED_TEMPERATURE += 1
-    displaySetPoint(DESIRED_TEMPERATURE)
+    global desired_temperature
+    if (desired_temperature < 100):
+        desired_temperature += 1
+        setSetPoint(desired_temperature)
+    displaySetPoint(desired_temperature)
 
 def decrease():
-    global DESIRED_TEMPERATURE
-    if (DESIRED_TEMPERATURE > 0):
-        DESIRED_TEMPERATURE -= 1
-    displaySetPoint(DESIRED_TEMPERATURE)
+    global desired_temperature
+    if (desired_temperature > 0):
+        desired_temperature -= 1
+        setSetPoint(desired_temperature)
+    displaySetPoint(desired_temperature)
 
 def displaySetPoint(setpoint: int):
     lbl_setpoint["text"] = f'Set Point = {setpoint:d}' + degree_sign
+
+ini_file = './config.ini'
+choices = configparser.ConfigParser()
+
+def getSetPoint() -> int:
+    choices.read(ini_file)
+    return choices.getint('configuration', 'set_point')
+
+def setSetPoint(setpoint:int):
+    config_file = open(ini_file, "w")
+    choices.set('configuration', 'set_point', str(setpoint))
+    choices.write(config_file)
+    config_file.close()
+
+def getPidKValue(k_value:str) -> float:
+    choices.read(ini_file)
+    return choices.getfloat('configuration', 'k' + k_value)
 
 if __name__ == "__main__":
     try:
         mywindow = tk.Tk()
         mywindow.title('Fan Control')
         #mywindow.geometry('50x16+0+36')
+
+        desired_temperature = getSetPoint()
         global lbl_setpoint
-        lbl_setpoint = tk.Label(text=f'Set Point = {DESIRED_TEMPERATURE:d}' + degree_sign, font=("Arial 14 bold"))
+        lbl_setpoint = tk.Label(text=f'Set Point = {desired_temperature:d}' + degree_sign, font=("Arial 14 bold"))
         lbl_setpoint.pack()
 
         btn_decrease = tk.Button(master=mywindow, text=down_arrow, command=decrease)
@@ -106,20 +140,25 @@ if __name__ == "__main__":
         lbl_duty_cycle = tk.Label(text="Duty Cycle = 0", font=("Arial 14 bold"))
         lbl_duty_cycle.pack()
         global lbl_rpm
-        lbl_rpm = tk.Label(text="RPMs = 0", font=("Arial 14 bold"))
+        lbl_rpm = tk.Label(text="Fan Running at 0 rpm", font=("Arial 14 bold"))
         lbl_rpm.pack()
         #tk.Button(text="quit", font=('Courier', 18), command=quit).pack()
         #mywindow.overrideredirect(1)
 
+        # Start up the fan controller thread
         is_running = True
         controlLoopThread = threading.Thread(target=controlLoop)
         controlLoopThread.start()
 
+        # Start up the window's event loop...will not return until the window is closed
         mywindow.mainloop()
-        print('Finished')
+        print('Exiting Mainloop')
     except KeyboardInterrupt:
         print('Interrupted')
 
+    # Stop the fan controller thread and wait for it to exit
     is_running = False
-    if (controlLoopThread.isAlive == True):
-        controlLoopThread.join(2.0)
+    print('Waiting for Control Loop to Exit')
+    controlLoopThread.join(2.0)
+
+    print('Exiting Program')
