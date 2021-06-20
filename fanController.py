60fd219dd2496054947601b04d74a02e24006673
Craig McCoy <craig.graham.mccoy@gmail.com>
2021-06-19 21:30:26 -0600

    Added more UI functionality.

--- fanController.py
+++ fanController.py
@@ -37,38 +37,68 @@ def controlLoop():
     # Set up PWM on the Fan Pin
     pwm = GPIO.PWM(FAN_PWM_PIN, FAN_PWM_FEQUENCY)
     pwm.start(0)
+
+    global tach_counter
+    tach_counter = 0  
     # Attach the Tach Falling Edge Event method
     GPIO.add_event_detect(TACH_PIN, GPIO.FALLING, TachFallingEdgeDetectedEvent)
 
     global is_running
     while is_running:
-        global tach_counter
-        tach_counter = 0        
+      
         sleep(1)
         currentTemperature = cpu.temperature
         #print(f'Temperature = {currentTemperature:.2f}' + degree_sign + 'C')
         lbl_temperature["text"] = f'Temperature = {currentTemperature:.2f}' + degree_sign + 'C'
+        pid.setpoint = DESIRED_TEMPERATURE
         fan_duty_cycle = pid(currentTemperature)
         #print(f'Duty Cycle = {fan_duty_cycle:.0f}%')
         lbl_duty_cycle["text"] = f'Duty Cycle = {fan_duty_cycle:.0f}%'
         pwm.ChangeDutyCycle(fan_duty_cycle)
         #print(f'RPMs = {tach_counter * 30:d} rpm')
         lbl_rpm["text"] = f'RPMs = {tach_counter * 30:d} rpm'
+        tach_counter = 0  
 
     GPIO.remove_event_detect(TACH_PIN)
     GPIO.cleanup()
     print("Exiting the Control Loop")
 
+def increase():
+    global DESIRED_TEMPERATURE
+    if (DESIRED_TEMPERATURE < 100):
+        DESIRED_TEMPERATURE += 1
+    displaySetPoint(DESIRED_TEMPERATURE)
+
+def decrease():
+    global DESIRED_TEMPERATURE
+    if (DESIRED_TEMPERATURE > 0):
+        DESIRED_TEMPERATURE -= 1
+    displaySetPoint(DESIRED_TEMPERATURE)
+
+def displaySetPoint(setpoint: int):
+    lbl_setpoint["text"] = f'Set Point = {setpoint:d}' + degree_sign + 'C'
+
 if __name__ == "__main__":
-    is_running = True;
+    is_running = True
     controlLoopThread = threading.Thread(target=controlLoop)
-    controlLoopThread.start();
+    controlLoopThread.start()
 
     try:
         mywindow = tk.Tk()
         mywindow.title('Fan Control')
         #mywindow.geometry('50x16+0+36')
-        tk.Label(text=f'Set Point = {DESIRED_TEMPERATURE:d}' + degree_sign + 'C', font=("Arial 14 bold")).pack()
+        global lbl_setpoint
+        lbl_setpoint = tk.Label(text=f'Set Point = {DESIRED_TEMPERATURE:d}' + degree_sign + 'C', font=("Arial 14 bold"))
+        lbl_setpoint.pack()
+
+        btn_decrease = tk.Button(master=mywindow, text="-", command=decrease)
+        #btn_decrease.grid(row=0, column=0, sticky="nsew")
+        btn_decrease.pack()
+
+        btn_increase = tk.Button(master=mywindow, text="+", command=increase)
+        #btn_increase.grid(row=0, column=2, sticky="nsew")
+        btn_increase.pack()
+
         global lbl_temperature
         lbl_temperature = tk.Label(text="Temperature = 0", font=("Arial 14 bold"))
         lbl_temperature.pack()
@@ -85,5 +115,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print('Interrupted')
 
-    is_running = False;
-    controlLoopThread.join(1.2)
+    is_running = False
+    if (controlLoopThread.isAlive == True):
+        controlLoopThread.join(2.0)
